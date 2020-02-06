290427d5545efa06945869efd5d668c2f00844c7
ppdraga <ppdraga@mail.ru>
2020-02-06 22:17:59 +0300

    add health check test

--- chronocraty/chronocraty/urls.py
+++ chronocraty/chronocraty/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import core.views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health-check/', core_views.healthcheck)
 ]
--- chronocraty/core/tests.py
+++ chronocraty/core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+
+
+class TestHealthCheck(TestCase):
+    def setUp(self):
+        self.assertEqual.__self__.maxDiff = None
+
+    def test_health_check(self):
+        client = Client()
+        response = client.get('/health-check/', {})
+        result = response.content.decode()
+        print(result)
+        self.assertEqual('Health-check OK', result)
+
+
+
