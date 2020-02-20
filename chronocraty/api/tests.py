acb62d5796adc6ef889aa3936518350e0f9232e3
Близнюк Ярослав Сергеевич <Bliznyuk.Yaroslav@svyaznoy.ru>
2020-02-20 18:05:06 +0300

    add TestUsersAPI

--- chronocraty/api/tests.py
+++ chronocraty/api/tests.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.test import APIRequestFactory, APIClient
 
 
-from api.web.v1.views import TaskListCreateAPIView
+from api.web.v1.views import TaskListCreateAPIView, UserListCreateAPIView
 from core.models import Task, User
 
 
@@ -39,3 +39,41 @@ class TestTasksAPI(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Task.objects.count(), 1)
         self.assertEqual(Task.objects.get().title, 'Task1')
+
+
+class TestUsersAPI(TestCase):
+    def setUp(self):
+        User.objects.create_user('mail@mail.ru', password='password', is_active=True, is_staff=True, is_admin=True)
+        self.assertEqual.__self__.maxDiff = None
+
+    def test_api_list_user(self):
+        user_id = User.objects.get(email='mail@mail.ru').id
+        factory = APIRequestFactory()
+        view = UserListCreateAPIView.as_view()
+        request = factory.get('/api/users/')
+        response = view(request)
+        received_data = json.loads(response.rendered_content.decode())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(received_data[0]['email'], 'mail@mail.ru')
+
+    def test_api_create_user(self):
+        factory = APIRequestFactory()
+        data = {
+            "password": "password",
+            "last_login": None,
+            "first_name": "John",
+            "last_name": "Smith",
+            "email": "JohnSmith@mail.ru",
+            "username": "John Smith",
+            "is_active": True,
+            "is_staff": True,
+            "is_admin": True,
+            "status": None
+        }
+        view = UserListCreateAPIView.as_view()
+        request = factory.post('/api/users/', json.dumps(data), content_type='application/json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(first_name='John').username, 'John Smith')
